84fe24160baa66916057700f3ea44c38a0812dff
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-17 20:26:23 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,15 +8,15 @@ class Pokemon
     @db = db 
   end 
   
-    def self.save
-    DB[:conn].execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
-  def self.find(id)
-    pokemon = DB[:conn].execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+  def self.find(id, database_connection)
+    pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
     name = pokemon[1]
     type = pokemon[2]
 
-    pokemon_inst = Pokemon.new(id: id, name: name, type: type, db: DB[:conn])
+    pokemon_inst = Pokemon.new(id: id, name: name, type: type, db: database_connection)
   end
 end 
