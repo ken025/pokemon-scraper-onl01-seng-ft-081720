96bbfb8e36147c580cde90ce11ddcc8c0b5f3dce
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-17 19:57:01 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+  attr_accessor :name, :type, :db, :id
+
+  def initialize(name:, type:, id:, db:)
+    @name = name
+    @type = type
+    @id = id 
+    @db = db 
+  end 
+  
+  def self.save
+    sql = "INSERT INTO songs (name, album)
+    VALUES(?,?)"
+    
+    DB
 end
